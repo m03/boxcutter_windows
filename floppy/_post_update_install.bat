189c00fec16852346d44d7fcba84186ebfc19309
Morrie Winnett <moe.winnett@gmail.com>
2016-06-23 09:31:51 -0700

    Add call to post update script

--- floppy/_post_update_install.bat
+++ floppy/_post_update_install.bat
@@ -2,4 +2,4 @@ ECHO OFF
 REM This script is not included on the floppy by default.
 REM If you're using update.bat, include this file if you
 REM want to run scripts following all updates/reboots.
-a:\zz-start-sshd.cmd
+call a:\zz-start-sshd.cmd
